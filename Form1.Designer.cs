fd50c1b1893b87aed74cb3b83328f190add561ef
EdgarMtzSndvl <edgar.martinezsvl@uanl.edu.mx>
2023-10-07 21:17:09 -0600

    antes d aplicar VB Powerpack

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,13 @@
             TXTB_Login_Email = new TextBox();
             TXTB_Login_Pass = new TextBox();
             BTN_Login_Entrar = new Button();
+            PANEL_Login = new Panel();
             SuspendLayout();
             // 
             // LBL_Login_Email
             // 
             LBL_Login_Email.AutoSize = true;
-            LBL_Login_Email.Location = new Point(181, 144);
+            LBL_Login_Email.Location = new Point(360, 143);
             LBL_Login_Email.Name = "LBL_Login_Email";
             LBL_Login_Email.Size = new Size(39, 15);
             LBL_Login_Email.TabIndex = 0;
@@ -47,7 +48,7 @@
             // LBL_Login_Pass
             // 
             LBL_Login_Pass.AutoSize = true;
-            LBL_Login_Pass.Location = new Point(160, 179);
+            LBL_Login_Pass.Location = new Point(339, 178);
             LBL_Login_Pass.Name = "LBL_Login_Pass";
             LBL_Login_Pass.Size = new Size(60, 15);
             LBL_Login_Pass.TabIndex = 1;
@@ -55,38 +56,52 @@
             // 
             // TXTB_Login_Email
             // 
-            TXTB_Login_Email.Location = new Point(228, 143);
+            TXTB_Login_Email.Location = new Point(407, 142);
             TXTB_Login_Email.Name = "TXTB_Login_Email";
             TXTB_Login_Email.Size = new Size(100, 23);
             TXTB_Login_Email.TabIndex = 2;
             // 
             // TXTB_Login_Pass
             // 
-            TXTB_Login_Pass.Location = new Point(228, 179);
+            TXTB_Login_Pass.Location = new Point(407, 178);
             TXTB_Login_Pass.Name = "TXTB_Login_Pass";
             TXTB_Login_Pass.Size = new Size(100, 23);
             TXTB_Login_Pass.TabIndex = 3;
             // 
             // BTN_Login_Entrar
             // 
-            BTN_Login_Entrar.Location = new Point(228, 217);
+            BTN_Login_Entrar.Location = new Point(407, 216);
             BTN_Login_Entrar.Name = "BTN_Login_Entrar";
             BTN_Login_Entrar.Size = new Size(75, 23);
             BTN_Login_Entrar.TabIndex = 4;
             BTN_Login_Entrar.Text = "Entrar";
             BTN_Login_Entrar.UseVisualStyleBackColor = true;
             // 
+            // PANEL_Login
+            // 
+            PANEL_Login.BackColor = Color.DarkRed;
+            PANEL_Login.Dock = DockStyle.Left;
+            PANEL_Login.Location = new Point(0, 0);
+            PANEL_Login.Name = "PANEL_Login";
+            PANEL_Login.Size = new Size(254, 391);
+            PANEL_Login.TabIndex = 5;
+            // 
             // FORM_Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Black;
+            ClientSize = new Size(762, 391);
+            Controls.Add(PANEL_Login);
             Controls.Add(BTN_Login_Entrar);
             Controls.Add(TXTB_Login_Pass);
             Controls.Add(TXTB_Login_Email);
             Controls.Add(LBL_Login_Pass);
             Controls.Add(LBL_Login_Email);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "FORM_Login";
+            Opacity = 0.8D;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
             ResumeLayout(false);
             PerformLayout();
@@ -99,5 +114,6 @@
         private TextBox TXTB_Login_Email;
         private TextBox TXTB_Login_Pass;
         private Button BTN_Login_Entrar;
+        private Panel PANEL_Login;
     }
 }

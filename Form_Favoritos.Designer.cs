cacde17aa7e7efd499bc666cb7bdaf01babda78d
JoseDelacruzS <jose.delacruzsnt@uanl.edu.mx>
2023-10-08 21:53:40 -0600

    Inicio de ventana favoritos

---
+++ Form_Favoritos.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MAD
+{
+    partial class Form_Favoritos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Favoritos));
+            SuspendLayout();
+            // 
+            // Form_Favoritos
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Beige;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form_Favoritos";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}

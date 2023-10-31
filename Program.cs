ace89b63cdfe5afd310c0b101538dfc662f2f015
EdgarMtzSndvl <edgar.martinezsvl@uanl.edu.mx>
2023-10-30 22:35:49 -0600

    se agrego la cadena de conexion y la funcion de login

--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
+using Microsoft.Extensions.Configuration;
+
 namespace MAD
 {
     internal static class Program
     {
+        public static IConfiguration? Configuration;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional:true, reloadOnChange: true);
+            Configuration = builder.Build();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new FORM_Login());
         }
+
     }
+
 }

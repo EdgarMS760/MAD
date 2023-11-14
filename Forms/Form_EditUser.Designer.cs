0f01ed06bcdfc425e5e89bb70167411d2f5fa759
JoseDelacruzS <jose.delacruzsnt@uanl.edu.mx>
2023-11-14 01:24:57 -0600

    Baja logica, procedure contraseña con funcion

--- Forms/Form_EditUser.Designer.cs
+++ Forms/Form_EditUser.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            Pic_EditUser_ClickExit = new PictureBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -41,35 +40,30 @@
             RB_EditUser_Inactivo = new RadioButton();
             BTN_EditUser_Editar = new Button();
             TXTB_EditUser_Name = new TextBox();
-            TXTB_EditUser_Genere = new TextBox();
-            TXTB_EditUser_Email = new TextBox();
             BTN_EditUser_Guardar = new Button();
             DTP_EditUser_BirthDate = new DateTimePicker();
             CHEKB_EditUser_Tipo = new CheckBox();
+            label8 = new Label();
+            RB_EditUser_Female = new RadioButton();
+            RB_EditUser_Male = new RadioButton();
+            BTN_EditUser_CambiarContra = new Button();
+            BTN_EditUser_CambiarCorreo = new Button();
+            LBL_EditUser_MostrarCorreo = new Label();
+            LBL_EditUser_MostrarContraseña = new Label();
+            BTN_EditUser_Baja = new Button();
+            label9 = new Label();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)Pic_EditUser_ClickExit).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.NavajoWhite;
-            panel1.Controls.Add(Pic_EditUser_ClickExit);
             panel1.Controls.Add(label1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(765, 100);
             panel1.TabIndex = 0;
-            // 
-            // Pic_EditUser_ClickExit
-            // 
-            Pic_EditUser_ClickExit.Location = new Point(735, 3);
-            Pic_EditUser_ClickExit.Name = "Pic_EditUser_ClickExit";
-            Pic_EditUser_ClickExit.Size = new Size(27, 26);
-            Pic_EditUser_ClickExit.SizeMode = PictureBoxSizeMode.Zoom;
-            Pic_EditUser_ClickExit.TabIndex = 1;
-            Pic_EditUser_ClickExit.TabStop = false;
-            Pic_EditUser_ClickExit.Click += Pic_EditUser_ClickExit_Click;
             // 
             // label1
             // 
@@ -131,7 +125,7 @@
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label6.ForeColor = Color.Gray;
-            label6.Location = new Point(211, 388);
+            label6.Location = new Point(211, 432);
             label6.Name = "label6";
             label6.Size = new Size(91, 25);
             label6.TabIndex = 5;
@@ -142,7 +136,7 @@
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label7.ForeColor = Color.Gray;
-            label7.Location = new Point(244, 425);
+            label7.Location = new Point(244, 469);
             label7.Name = "label7";
             label7.Size = new Size(58, 25);
             label7.TabIndex = 6;
@@ -154,7 +148,7 @@
             RB_EditUser_Activo.Enabled = false;
             RB_EditUser_Activo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             RB_EditUser_Activo.ForeColor = Color.DimGray;
-            RB_EditUser_Activo.Location = new Point(308, 388);
+            RB_EditUser_Activo.Location = new Point(308, 432);
             RB_EditUser_Activo.Name = "RB_EditUser_Activo";
             RB_EditUser_Activo.Size = new Size(71, 25);
             RB_EditUser_Activo.TabIndex = 7;
@@ -168,7 +162,7 @@
             RB_EditUser_Inactivo.Enabled = false;
             RB_EditUser_Inactivo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             RB_EditUser_Inactivo.ForeColor = Color.DimGray;
-            RB_EditUser_Inactivo.Location = new Point(385, 389);
+            RB_EditUser_Inactivo.Location = new Point(385, 433);
             RB_EditUser_Inactivo.Name = "RB_EditUser_Inactivo";
             RB_EditUser_Inactivo.Size = new Size(82, 25);
             RB_EditUser_Inactivo.TabIndex = 8;
@@ -202,36 +196,15 @@
             TXTB_EditUser_Name.Size = new Size(242, 33);
             TXTB_EditUser_Name.TabIndex = 10;
             // 
-            // TXTB_EditUser_Genere
-            // 
-            TXTB_EditUser_Genere.BackColor = Color.LemonChiffon;
-            TXTB_EditUser_Genere.Enabled = false;
-            TXTB_EditUser_Genere.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            TXTB_EditUser_Genere.ForeColor = Color.DimGray;
-            TXTB_EditUser_Genere.Location = new Point(308, 300);
-            TXTB_EditUser_Genere.Name = "TXTB_EditUser_Genere";
-            TXTB_EditUser_Genere.Size = new Size(242, 33);
-            TXTB_EditUser_Genere.TabIndex = 12;
-            // 
-            // TXTB_EditUser_Email
-            // 
-            TXTB_EditUser_Email.BackColor = Color.LemonChiffon;
-            TXTB_EditUser_Email.Enabled = false;
-            TXTB_EditUser_Email.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            TXTB_EditUser_Email.ForeColor = Color.DimGray;
-            TXTB_EditUser_Email.Location = new Point(308, 342);
-            TXTB_EditUser_Email.Name = "TXTB_EditUser_Email";
-            TXTB_EditUser_Email.Size = new Size(242, 33);
-            TXTB_EditUser_Email.TabIndex = 13;
-            // 
             // BTN_EditUser_Guardar
             // 
             BTN_EditUser_Guardar.BackColor = Color.NavajoWhite;
+            BTN_EditUser_Guardar.Enabled = false;
             BTN_EditUser_Guardar.FlatAppearance.BorderColor = Color.NavajoWhite;
             BTN_EditUser_Guardar.FlatStyle = FlatStyle.Flat;
             BTN_EditUser_Guardar.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             BTN_EditUser_Guardar.ForeColor = Color.DimGray;
-            BTN_EditUser_Guardar.Location = new Point(319, 527);
+            BTN_EditUser_Guardar.Location = new Point(319, 534);
             BTN_EditUser_Guardar.Name = "BTN_EditUser_Guardar";
             BTN_EditUser_Guardar.Size = new Size(108, 33);
             BTN_EditUser_Guardar.TabIndex = 14;
@@ -258,12 +231,129 @@
             CHEKB_EditUser_Tipo.Enabled = false;
             CHEKB_EditUser_Tipo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             CHEKB_EditUser_Tipo.ForeColor = Color.DimGray;
-            CHEKB_EditUser_Tipo.Location = new Point(308, 427);
+            CHEKB_EditUser_Tipo.Location = new Point(308, 471);
             CHEKB_EditUser_Tipo.Name = "CHEKB_EditUser_Tipo";
             CHEKB_EditUser_Tipo.Size = new Size(129, 25);
             CHEKB_EditUser_Tipo.TabIndex = 16;
             CHEKB_EditUser_Tipo.Text = "Administrador";
             CHEKB_EditUser_Tipo.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.Gray;
+            label8.Location = new Point(163, 389);
+            label8.Name = "label8";
+            label8.Size = new Size(141, 25);
+            label8.TabIndex = 17;
+            label8.Text = "CONTRASEÑA:";
+            // 
+            // RB_EditUser_Female
+            // 
+            RB_EditUser_Female.AutoSize = true;
+            RB_EditUser_Female.Enabled = false;
+            RB_EditUser_Female.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            RB_EditUser_Female.ForeColor = Color.DimGray;
+            RB_EditUser_Female.Location = new Point(413, 307);
+            RB_EditUser_Female.Name = "RB_EditUser_Female";
+            RB_EditUser_Female.Size = new Size(97, 25);
+            RB_EditUser_Female.TabIndex = 20;
+            RB_EditUser_Female.TabStop = true;
+            RB_EditUser_Female.Text = "Femenino";
+            RB_EditUser_Female.UseVisualStyleBackColor = true;
+            // 
+            // RB_EditUser_Male
+            // 
+            RB_EditUser_Male.AutoSize = true;
+            RB_EditUser_Male.Enabled = false;
+            RB_EditUser_Male.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            RB_EditUser_Male.ForeColor = Color.DimGray;
+            RB_EditUser_Male.Location = new Point(308, 307);
+            RB_EditUser_Male.Name = "RB_EditUser_Male";
+            RB_EditUser_Male.Size = new Size(99, 25);
+            RB_EditUser_Male.TabIndex = 19;
+            RB_EditUser_Male.TabStop = true;
+            RB_EditUser_Male.Text = "Masculino";
+            RB_EditUser_Male.UseVisualStyleBackColor = true;
+            // 
+            // BTN_EditUser_CambiarContra
+            // 
+            BTN_EditUser_CambiarContra.BackColor = Color.NavajoWhite;
+            BTN_EditUser_CambiarContra.FlatAppearance.BorderColor = Color.NavajoWhite;
+            BTN_EditUser_CambiarContra.FlatStyle = FlatStyle.Flat;
+            BTN_EditUser_CambiarContra.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BTN_EditUser_CambiarContra.ForeColor = Color.DimGray;
+            BTN_EditUser_CambiarContra.Location = new Point(556, 381);
+            BTN_EditUser_CambiarContra.Name = "BTN_EditUser_CambiarContra";
+            BTN_EditUser_CambiarContra.Size = new Size(99, 33);
+            BTN_EditUser_CambiarContra.TabIndex = 21;
+            BTN_EditUser_CambiarContra.Text = "Cambiar";
+            BTN_EditUser_CambiarContra.UseVisualStyleBackColor = false;
+            BTN_EditUser_CambiarContra.Click += BTN_EditUser_CambiarContra_Click;
+            // 
+            // BTN_EditUser_CambiarCorreo
+            // 
+            BTN_EditUser_CambiarCorreo.BackColor = Color.NavajoWhite;
+            BTN_EditUser_CambiarCorreo.FlatAppearance.BorderColor = Color.NavajoWhite;
+            BTN_EditUser_CambiarCorreo.FlatStyle = FlatStyle.Flat;
+            BTN_EditUser_CambiarCorreo.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BTN_EditUser_CambiarCorreo.ForeColor = Color.DimGray;
+            BTN_EditUser_CambiarCorreo.Location = new Point(556, 341);
+            BTN_EditUser_CambiarCorreo.Name = "BTN_EditUser_CambiarCorreo";
+            BTN_EditUser_CambiarCorreo.Size = new Size(99, 33);
+            BTN_EditUser_CambiarCorreo.TabIndex = 22;
+            BTN_EditUser_CambiarCorreo.Text = "Cambiar";
+            BTN_EditUser_CambiarCorreo.UseVisualStyleBackColor = false;
+            BTN_EditUser_CambiarCorreo.Click += BTN_EditUser_CambiarCorreo_Click;
+            // 
+            // LBL_EditUser_MostrarCorreo
+            // 
+            LBL_EditUser_MostrarCorreo.AutoSize = true;
+            LBL_EditUser_MostrarCorreo.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            LBL_EditUser_MostrarCorreo.ForeColor = Color.Gray;
+            LBL_EditUser_MostrarCorreo.Location = new Point(308, 350);
+            LBL_EditUser_MostrarCorreo.Name = "LBL_EditUser_MostrarCorreo";
+            LBL_EditUser_MostrarCorreo.Size = new Size(70, 25);
+            LBL_EditUser_MostrarCorreo.TabIndex = 23;
+            LBL_EditUser_MostrarCorreo.Text = "Correo";
+            // 
+            // LBL_EditUser_MostrarContraseña
+            // 
+            LBL_EditUser_MostrarContraseña.AutoSize = true;
+            LBL_EditUser_MostrarContraseña.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            LBL_EditUser_MostrarContraseña.ForeColor = Color.Gray;
+            LBL_EditUser_MostrarContraseña.Location = new Point(308, 389);
+            LBL_EditUser_MostrarContraseña.Name = "LBL_EditUser_MostrarContraseña";
+            LBL_EditUser_MostrarContraseña.Size = new Size(109, 25);
+            LBL_EditUser_MostrarContraseña.TabIndex = 24;
+            LBL_EditUser_MostrarContraseña.Text = "Contraseña";
+            // 
+            // BTN_EditUser_Baja
+            // 
+            BTN_EditUser_Baja.BackColor = Color.NavajoWhite;
+            BTN_EditUser_Baja.FlatAppearance.BorderColor = Color.NavajoWhite;
+            BTN_EditUser_Baja.FlatStyle = FlatStyle.Flat;
+            BTN_EditUser_Baja.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BTN_EditUser_Baja.ForeColor = Color.DimGray;
+            BTN_EditUser_Baja.Location = new Point(645, 642);
+            BTN_EditUser_Baja.Name = "BTN_EditUser_Baja";
+            BTN_EditUser_Baja.Size = new Size(108, 33);
+            BTN_EditUser_Baja.TabIndex = 25;
+            BTN_EditUser_Baja.Text = "BAJA";
+            BTN_EditUser_Baja.UseVisualStyleBackColor = false;
+            BTN_EditUser_Baja.Click += BTN_EditUser_Baja_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.ForeColor = Color.Gray;
+            label9.Location = new Point(411, 646);
+            label9.Name = "label9";
+            label9.Size = new Size(235, 25);
+            label9.TabIndex = 26;
+            label9.Text = "DAR DE BAJA LA CUENTA";
             // 
             // Form_EditUser
             // 
@@ -271,11 +361,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Beige;
             ClientSize = new Size(765, 687);
+            Controls.Add(label9);
+            Controls.Add(BTN_EditUser_Baja);
+            Controls.Add(LBL_EditUser_MostrarContraseña);
+            Controls.Add(LBL_EditUser_MostrarCorreo);
+            Controls.Add(BTN_EditUser_CambiarCorreo);
+            Controls.Add(BTN_EditUser_CambiarContra);
+            Controls.Add(RB_EditUser_Female);
+            Controls.Add(RB_EditUser_Male);
+            Controls.Add(label8);
             Controls.Add(CHEKB_EditUser_Tipo);
             Controls.Add(DTP_EditUser_BirthDate);
             Controls.Add(BTN_EditUser_Guardar);
-            Controls.Add(TXTB_EditUser_Email);
-            Controls.Add(TXTB_EditUser_Genere);
             Controls.Add(TXTB_EditUser_Name);
             Controls.Add(BTN_EditUser_Editar);
             Controls.Add(RB_EditUser_Inactivo);
@@ -293,7 +390,6 @@
             Load += Form1_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)Pic_EditUser_ClickExit).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -302,7 +398,6 @@
 
         private Panel panel1;
         private Label label1;
-        private PictureBox Pic_EditUser_ClickExit;
         private Label label2;
         private Label label3;
         private Label label4;
@@ -313,10 +408,17 @@
         private RadioButton RB_EditUser_Inactivo;
         private Button BTN_EditUser_Editar;
         private TextBox TXTB_EditUser_Name;
-        private TextBox TXTB_EditUser_Genere;
-        private TextBox TXTB_EditUser_Email;
         private Button BTN_EditUser_Guardar;
         private DateTimePicker DTP_EditUser_BirthDate;
         private CheckBox CHEKB_EditUser_Tipo;
+        private Label label8;
+        private RadioButton RB_EditUser_Female;
+        private RadioButton RB_EditUser_Male;
+        private Button BTN_EditUser_CambiarContra;
+        private Button BTN_EditUser_CambiarCorreo;
+        private Label LBL_EditUser_MostrarCorreo;
+        private Label LBL_EditUser_MostrarContraseña;
+        private Button BTN_EditUser_Baja;
+        private Label label9;
     }
 }

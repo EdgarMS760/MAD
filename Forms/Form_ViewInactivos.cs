49089995197e7c3252895eb037a9d2c86ad1db24
EdgarMtzSndvl <edgar.martinezsvl@uanl.edu.mx>
2023-11-20 22:12:57 -0600

    se pueden ver usuarios inactivos

---
+++ Forms/Form_ViewInactivos.cs
@@ -0,0 +1,41 @@
+﻿using MAD.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAD.Forms
+{
+    public partial class Form_ViewInactivos : Form
+    {
+        Usuarios _UsuarioSrvs = Usuarios.GetInstance();
+        public Form_ViewInactivos()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_ViewInactivos_Load(object sender, EventArgs e)
+        {
+            List<inactivosDto> inactivos = _UsuarioSrvs.viewInactivos();
+            if (inactivos.Count>0)
+            {
+             dataGrid_Inactivos.DataSource = inactivos;
+            }
+            else
+            {
+                MessageBox.Show("no hay usuarios inactivos", "ATENCION",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+          
+        }
+
+        private void dataGrid_Inactivos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
--- Services/SesionUsuario.cs
+++ Services/SesionUsuario.cs
@@ -13,11 +13,11 @@ namespace MAD.Services
     {
         public static string CorreoElectronico { get; set; }
         public static string Contrasena { get; set; }
-        public static string NombreCom { get; set; }  
-        public static char Genero { get; set; }  
-        public static DateTime FechaNac { get; set; }  
-        public static bool Tipo { get; set; }  
-        public static string Estatus { get; set; }  
+        public static string NombreCom { get; set; }
+        public static char Genero { get; set; }
+        public static DateTime FechaNac { get; set; }
+        public static bool Tipo { get; set; }
+        public static string Estatus { get; set; }
     }
 
     public static class gestionUsuariosDto
@@ -25,5 +25,11 @@ namespace MAD.Services
         public static string email { get; set; }
         public static bool adminOrNot { get; set; }
         public static string estado { get; set; }
+    }
+
+    public class inactivosDto
+    {
+        public  string EMAIL { get; set; }
+        public  string ESTADO { get; set; }
     }
 }

f8fb1d4b8fcfa39964f7f7ad5f96a5df2d2ca5fc
EdgarMtzSndvl <edgar.martinezsvl@uanl.edu.mx>
2023-11-21 00:04:35 -0600

    ahora se puede ingresar con pass temporal si el usuario esta inactivo

--- Querys/Procedure_CrearUsuario-Trigger_InfoPersonal.sql
+++ Querys/Procedure_CrearUsuario-Trigger_InfoPersonal.sql
@@ -4,30 +4,30 @@ ALTER PROCEDURE sp_AltaUsuario
     @UsuarioExistente BIT OUTPUT
 AS
 BEGIN
-    -- Iniciar una transacción para asegurar la consistencia de los datos
+
     BEGIN TRANSACTION
 
-    -- Verificar si el usuario ya existe
+
     IF EXISTS (SELECT 1 FROM Usuario WHERE CorreoElectronico = @Correo)
     BEGIN
-        -- El usuario ya existe, establecer @UsuarioExistente en 1 (verdadero)
+
         SET @UsuarioExistente = 1;
     END
     ELSE
     BEGIN
-        -- El usuario no existe, proceder con la inserción en la tabla Usuario
+
         INSERT INTO Usuario (CorreoElectronico) 
         VALUES (@Correo);
 
-        -- Insertar en la tabla de contraseñas
+
         INSERT INTO Contrasena (CorreoElectronico, ContrasenaActual, ContrasenaTemporal, ContrasenaAnterior1, ContrasenaAnterior2)
         VALUES (@Correo, @ContrasenaActual, NULL, NULL, NULL);
 
-        -- El usuario no existe, establecer @UsuarioExistente en 0 (falso)
+
         SET @UsuarioExistente = 0;
     END
 
-    -- Confirmar la transacción
+
     COMMIT TRANSACTION
 END
 
@@ -36,7 +36,7 @@ ON Usuario
 AFTER INSERT
 AS
 BEGIN
-    -- Insertar en la tabla InfoPersonal
+
     INSERT INTO InfoPersonal (CorreoElectronico)
     SELECT CorreoElectronico
     FROM inserted;
--- Services/LoginServices.cs
+++ Services/LoginServices.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -58,12 +59,12 @@ namespace MAD.Services
                     case 2:
                         {
                             MessageBox.Show("contraseña incorrecta", "Error de autenticacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            // Incrementar el contador de intentos fallidos
+
                             _UsuarioSrvs.IncrementarIntentosFallidos(email);
                             int intentosFallidos = _UsuarioSrvs.ObtenerIntentosFallidos(email);
                             if (intentosFallidos >= 3)
                             {
-                                // Desactivar al usuario después de tres intentos fallidos
+
                                 _UsuarioSrvs.DesactivarUsuario(email);
                             }
                         }
@@ -76,12 +77,95 @@ namespace MAD.Services
                     default:
                         break;
                 }
-             
+
                 connection.Close();
                 return autenticado;
 
             }
 
+        }
+        public int autenticacionPassTemporal(string email, string pass)
+        {
+            if (connectionString == null) { return 4; }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT dbo.AutenticarUsuarioPassTemporal(@Correo, @Contrasena)", connection);
+                command.Parameters.AddWithValue("@Correo", email);
+                command.Parameters.AddWithValue("@Contrasena", pass);
+
+                int autenticado = (int)command.ExecuteScalar();
+
+                switch (autenticado)
+                {
+                    case 1:
+                        {
+                            MessageBox.Show("FAVOR DE CAMBIAR SU CONTRASEÑA DESDE EL PANEL DE CONFIGURACION ", "ATENCION", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            SesionUsuario.CorreoElectronico = email;
+                            SesionUsuario.Contrasena = pass;
+                            _UsuarioSrvs.ObtenerInfoUsuario(SesionUsuario.CorreoElectronico);
+                            _UsuarioSrvs.ReiniciarIntentosFallidos(email);
+                        }
+                        break;
+                    case 2:
+                        {
+                            MessageBox.Show("contraseña temporal incorrecta", "Error de autenticacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            MessageBox.Show("correo electronico incorrecto", "Error de autenticacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                connection.Close();
+                return autenticado;
+
+            }
+
+        }
+        public string validacionEstado(string email)
+        {
+            string resultado = null;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand("ProcViewEstadoUsuario", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.AddWithValue("@CorreoElectronico", email);
+                    object result = sqlCommand.ExecuteScalar();
+                    if (result != null)
+                    {
+                         resultado = result.ToString().Trim();
+                    }
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+            return resultado;
+        }
+        public void loginPassTemporal(string email,string pass)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand("loginPassTemporal", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.AddWithValue("@CorreoElectronico", email);
+                    sqlCommand.Parameters.AddWithValue("@Contrasena", pass);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
